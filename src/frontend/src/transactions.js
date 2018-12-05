2cbd7fbaa6adb880155f497513bc13566b8fccbe
andrey.bugakov <andrey.bugakov@billing.ru>
2018-12-06 00:56:46 +0400

    added react frontend

---
+++ src/frontend/src/transactions.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
+import '../node_modules/jquery/dist/jquery.slim.min.js';
+
+// import ReactDOM from 'react-dom';
+import Table from 'react-bootstrap/lib/Table';
+import Button from 'react-bootstrap/lib/Button';
+
+
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+
+class Transactions extends React.Component{
+    componentDidMount(){
+
+    }
+    render(){
+        return (
+        <div>
+          <Button class="btn-primary">Create Transactions</Button>
+          <Table responsive>
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>Table heading</th>
+      <th>Table heading</th>
+      <th>Table heading</th>
+      <th>Table heading</th>
+      <th>Table heading</th>
+      <th>Table heading</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td>Table cell</td>
+      <td>Table cell</td>
+      <td>Table cell</td>
+      <td>Table cell</td>
+      <td>Table cell</td>
+      <td>Table cell</td>
+    </tr>
+    <tr>
+      <td>2</td>
+      <td>Table cell</td>
+      <td>Table cell</td>
+      <td>Table cell</td>
+      <td>Table cell</td>
+      <td>Table cell</td>
+      <td>Table cell</td>
+    </tr>
+    <tr>
+      <td>3</td>
+      <td>Table cell</td>
+      <td>Table cell</td>
+      <td>Table cell</td>
+      <td>Table cell</td>
+      <td>Table cell</td>
+      <td>Table cell</td>
+    </tr>
+  </tbody>
+</Table>;
+        </div>
+        );}
+}
+export default Transactions;
